3678bc20ec9e2db5ad051dda23ee4afa583720aa
Markus Johansen <74660130+KusMar00@users.noreply.github.com>
2022-10-15 00:42:22 +0200

    board

---
+++ src/components/Board.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Letter from "./Letter";
+
+const Board = () => {
+  return (
+    <div className="Board">
+      <div className="Board-row">
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+      </div>
+      <div className="Board-row">
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+      </div>
+      <div className="Board-row">
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+      </div>
+      <div className="Board-row">
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+      </div>
+      <div className="Board-row">
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+        <Letter />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Letter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Letter = () => {
+  return (
+    <div className="Letter">
+      <p>A</p>
+    </div>
+  );
+};
+
+export default Letter;
